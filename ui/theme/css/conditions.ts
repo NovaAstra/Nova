db930937a25c8f7ded43420e1408818dc301da1a
NovaAstra <novaastra@outlook.com>
2023-12-15 11:25:56 +0800

    初始化

--- ui/theme/css/conditions.ts
+++
@@ -1,5 +0,0 @@
-import { breakpoints, SizeEnum } from "./breakpoints.js";
-
-export const responsivePropertiesConditions = breakpoints
-
-export const responsivePropertiesDefaultCondition = SizeEnum.XS;
